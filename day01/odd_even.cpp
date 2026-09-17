42ff6293b016042f621406c3f4473f08d568829d
j33390271-hue <j33390271@gmail.com>
2026-09-17 21:19:53 +0800

    day01:add odd even program

---
+++ day01/odd_even.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    if (a % 2 == 0)
+        cout << "此数为偶数";
+    else
+        cout << "此数为奇数";
+
+    return 0;
+}
